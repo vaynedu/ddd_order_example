e8a0dea774ad7b93ab60f3112777dfe9084af744
vaynedu <1219345363@qq.com>
2025-06-27 15:44:24 +0800

    refactor(数据库): 重构订单相关表结构并添加支付表
    - 删除并重建订单表、订单项表，添加支付表
    - 优化字段类型和注释，增加时间戳精度
    - 添加表引擎和字符集配置

--- internal/infrastructure/persistence/schema.sql
+++ internal/infrastructure/persistence/schema.sql
@@ -2,27 +2,44 @@
 CREATE DATABASE IF NOT EXISTS orders;
 USE orders;
 
+-- 删除表
+drop table t_order;
+drop table t_order_items;
+drop table t_payment;
+
 -- 创建订单表
 -- 订单主表，存储订单基本信息，与订单项表(t_order_items)为一对多关系
 CREATE TABLE IF NOT EXISTS t_order (
-    id VARCHAR(36) PRIMARY KEY,
-    customer_id VARCHAR(36) NOT NULL,
-    status ENUM('created', 'paid', 'shipped', 'completed', 'cancelled') NOT NULL,
-    total_amount INT NOT NULL COMMENT '订单总金额，单位：分',
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id VARCHAR(36) PRIMARY KEY COMMENT '主键id',
+    customer_id VARCHAR(36) NOT NULL COMMENT '客户id, todo感觉可以作为标识id',
+    status ENUM('created', 'paid', 'shipped', 'completed', 'cancelled') NOT NULL COMMENT '订单状态',
+    total_amount BIGINT(20) NOT NULL COMMENT '订单总金额，单位：分',
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间,精确到毫秒',
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '更新时间，精确到毫秒',
     INDEX idx_customer_id (customer_id),
     INDEX idx_status (status)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单表';
 
 -- 创建订单项表
 -- 订单项子表，存储订单包含的商品信息，通过order_id与订单主表关联
 CREATE TABLE IF NOT EXISTS t_order_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '主键id',
     order_id VARCHAR(36) NOT NULL COMMENT '关联订单主表的ID',
-    product_id VARCHAR(36) NOT NULL,
-    quantity INT NOT NULL,
-    unit_price INT NOT NULL COMMENT '商品单价，单位：分',
-    subtotal INT NOT NULL COMMENT '商品小计金额，单位：分',
+    product_id VARCHAR(36) NOT NULL COMMENT '商品id',
+    quantity BIGINT NOT NULL COMMENT '商品数量',
+    unit_price BIGINT(20) NOT NULL COMMENT '商品单价，单位：分',
+    subtotal BIGINT(20) NOT NULL COMMENT '商品小计金额，单位：分',
     INDEX idx_order_id (order_id)
-);
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='订单商品项表';
+
+-- 创建支付表
+-- 存储订单支付信息，通过order_id与订单主表关联
+CREATE TABLE IF NOT EXISTS t_payment (
+    id VARCHAR(36) PRIMARY KEY COMMENT '主键id',
+    order_id VARCHAR(36) NOT NULL COMMENT '关联订单主表的ID',
+    amount BIGINT NOT NULL COMMENT '支付金额，单位：分',
+    status ENUM('created', 'paid', 'refunded', 'failed', 'expired', 'canceled', 'refunding', 'refund_failed', 'refunded_success') NOT NULL COMMENT '支付状态',
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间,精确到毫秒',
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '更新时间，精确到毫秒',
+    INDEX idx_order_id (order_id)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='支付表';
